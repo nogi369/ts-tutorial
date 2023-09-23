ceddd740c98f8c7fe52d6b18be94fbd742c22db3
nogi <Createc369.Re@gmail.com>
2023-09-23 17:14:34 +0900

    TSでコードを書ける環境構築まで

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { type } from "os";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppProps = { message: string };
+
+function App({ message }: AppProps) {
+  return <div>{message}</div>;
 }
 
 export default App;
+
+{
+  /* <h2>TS + React</h2> */
+}
